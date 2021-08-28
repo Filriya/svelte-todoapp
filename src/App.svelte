9202d4b9d57ea1072aaa512a2d6dd8d92a580974
Filriya <arizoxppc@gmail.com>
2021-08-28 17:37:23 +0900

    todoapp

---
+++ src/App.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import TodoList from './TodoList.svelte'
+</script>
+
+<TodoList></TodoList>
+
+<style></style>
---
+++ src/TodoList.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  let items = ['taskA', 'taskB', 'taskC'];
+  let input = ""
+  const addItem = () => {
+    items = [...items, input]
+    input = ""
+  }
+  const deleteItem = (i) => {
+    items.splice(i, 1)
+    items = items
+  }
+</script>
+
+{#each items as item, i}
+  <div>{i} {item} <button on:click={() => deleteItem(i)}>x</button></div>
+{/each}
+
+{#if items.length === 0}
+  done!
+{/if}
+
+<input bind:value={input}/><button on:click={addItem}>add</button>
